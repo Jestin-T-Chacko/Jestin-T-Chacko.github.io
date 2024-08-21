a4b6fbea8d8b5e8e012f4ee285199e680b19194d
Adeeb Abubacker <148857537+adeebca@users.noreply.github.com>
2024-08-21 15:43:43 +0530

    testimonials updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,9 +6,10 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
+import flutter2 from "./flutter2.png";
 import css from "./tech/css.png";
 import flutter from "./tech/flutter.png";
+import contentCreator from "./content creator.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
@@ -28,12 +29,14 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.jpg";
-
+import ui_ux from "./ui_ux.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
+  ui_ux,
+  contentCreator,
   logo,
   backend,
   creator,
@@ -49,6 +52,7 @@ export {
   git,
   html,
   flutter,
+  flutter2,
   javascript,
   mongodb,
   nodejs,
